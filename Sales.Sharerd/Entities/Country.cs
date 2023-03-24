9dbdb3db8ceadec126be37926c331342dbfa2e0b
teo-o <mateoorozco313199@correo.itm.edu.co>
2023-03-23 21:22:56 -0500

    Add data context

---
+++ Sales.Sharerd/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede teber más de {1} caracteres")]
+        [Display(Name = "Country")]
+        public string Name { get; set; } = null!;
+
+    }
+}
