c5cb63733ed5c7814269bfd79fe3a531cbb3a038
teo-o <mateoorozco313199@correo.itm.edu.co>
2023-03-23 22:00:33 -0500

    Begin countries API controller

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -24,4 +24,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
 app.Run();
